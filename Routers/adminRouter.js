cfed4b4065c02d42d3e601b075c25bd95432f1d0
Tejaswini R J <140419352+tejaswini-2005@users.noreply.github.com>
2026-01-31 11:37:27 +0530

    Fix import statement for addUer in adminRouter

--- Routers/adminRouter.js
+++ Routers/adminRouter.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { addUer } from "../controller/aminControler.js";
+import { addUer } from "../controller/aminController.js";
 import { 
     adminMiddleware, 
     authMiddleware 
